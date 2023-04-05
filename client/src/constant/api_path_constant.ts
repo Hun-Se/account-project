481b34394a64d7db6df7c2dd8e62751607960540
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-04-05 23:10:43 +0900

    api 경로 및 정규화식 상수화하여 추가

---
+++ client/src/constant/api_path_constant.ts
@@ -0,0 +1,11 @@
+const API_PATH = {
+  LOGIN: "/api/users/login",
+  SIGNUP: "api/users/create",
+  TODOS: "/todos",
+  TODO_BY_ID: (id: string) => `/todos/${id}`,
+  CREATE_TODO: "/todos",
+  UPDATE_TODO: (id: string) => `/todos/${id}`,
+  DELETE_TODO: (id: string) => `/todos/${id}`,
+};
+
+export default API_PATH;
