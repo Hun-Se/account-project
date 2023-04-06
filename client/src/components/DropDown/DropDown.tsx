fbf0346eb1f78ef04c11087435e37726c9fc76b2
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-04-06 23:47:03 +0900

    DropDown 컴포넌트 생성

---
+++ client/src/components/DropDown/DropDown.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import classes from "./DropDown.module.css";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import {
+  showlist,
+  selectDropDownShown,
+} from "../../redux/dropdown/dropdownSlice";
+
+interface DropDownProps {
+  itemList: { id: number; name: string }[];
+}
+
+const DropDown = (props: DropDownProps) => {
+  const dropdownList = props.itemList.map((list) => (
+    <div className={classes["list-dropdown"]} key={list.id}>
+      {list.name}
+    </div>
+  ));
+
+  const isshown = useAppSelector(selectDropDownShown);
+  const dispatch = useAppDispatch();
+
+  const dropDownEventHandler = (event: React.MouseEvent) => {
+    event.preventDefault();
+    dispatch(showlist());
+  };
+
+  return (
+    <>
+      <div className={classes["dropdown"]}>
+        <button
+          className={classes["button-dropdown"]}
+          onClick={dropDownEventHandler}
+        >
+          <span className={classes["dropbtn_icon"]}></span>
+          <span className={classes["dropdown-category"]}>최신순</span>
+          <span className={classes["dropdown-title"]}>정렬</span>
+        </button>
+        {isshown && (
+          <ul className={classes["dropdown-list"]}>{dropdownList}</ul>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default DropDown;
