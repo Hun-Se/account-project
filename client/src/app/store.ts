dbede903564562d70b3f5b3569b26e603e519dd0
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-04-06 23:42:59 +0900

    dropdown reducer 와 modal reducer 추가

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -2,12 +2,16 @@ import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import counterReducer from "../redux/counter/counterSlice";
 import loginReducer from "../redux/auth/loginSlice";
 import signUpReducer from "../redux/auth/signUpSlice";
+import dropDownReducer from "../redux/dropdown/dropdownSlice";
+import modalReducer from "../redux/modal/modalSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     login: loginReducer,
     signup: signUpReducer,
+    dropdown: dropDownReducer,
+    modal: modalReducer,
   },
 });
 
