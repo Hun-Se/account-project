706c6aaeacf2f44aa36c1a0dddd9c962f93f107a
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-04-12 21:34:36 +0900

    Edit 버튼 기능 추가

--- client/src/components/Account/AccountListItem.tsx
+++ client/src/components/Account/AccountListItem.tsx
@@ -1,15 +1,20 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { useNavigate } from "react-router-dom";
-import { accountDtailModalShown } from "../../redux/modal/modalSlice";
+import {
+  accountDtailModalShown,
+  accountEditModalShown,
+} from "../../redux/modal/modalSlice";
+import { accountDeleteAsync } from "../../redux/account/accountDeleteSlice";
 import { AccountType } from "../../types/account";
 import classes from "./AccountList.module.css";
 
 const AccountListItem = (props: AccountType) => {
-  const dispatch = useDispatch();
+  const [data, setData] = useState(true);
+  const dispatch = useAppDispatch();
   const navigate = useNavigate();
 
-  const total = Number(props.income) + Number(props.expend);
+  const totalSales = Number(props.income) - Number(props.expend);
 
   const onDtailHandler = (event: React.MouseEvent) => {
     event.preventDefault();
@@ -17,24 +22,48 @@ const AccountListItem = (props: AccountType) => {
     navigate(`/account/${props._id}`);
   };
 
+  const onEditHandler = (event: React.MouseEvent) => {
+    event.preventDefault();
+    dispatch(accountEditModalShown());
+    navigate(`/account/edit/${props._id}`);
+  };
+
+  const onRemoveHandler = (event: React.MouseEvent) => {
+    event.preventDefault();
+    dispatch(accountDeleteAsync(props._id));
+    setData(false);
+  };
+
   return (
     <>
-      <li className={classes["container-list"]}>
-        <div className={classes["container-function-button"]}>
-          <button
-            className={classes["button-dtail"]}
-            onClick={onDtailHandler}
-          ></button>
-          <button className={classes["button-edit"]}></button>
-          <button className={classes["button-remove"]}></button>
-        </div>
-        <span className={classes["list-title"]}>{props.date}</span>
-        <span className={classes["list-content"]}>{props.item}</span>
-        <span className={classes["container-todo-button"]}>{props.income}</span>
-        <span className={classes["container-todo-button"]}>{props.expend}</span>
-        {total ? <span>{total}</span> : <></>}
-        <span>{props.memo}</span>
-      </li>
+      {data && (
+        <li className={classes["container-list"]}>
+          <div className={classes["container-function-button"]}>
+            <button
+              className={classes["button-dtail"]}
+              onClick={onDtailHandler}
+            ></button>
+            <button
+              className={classes["button-edit"]}
+              onClick={onEditHandler}
+            ></button>
+            <button
+              className={classes["button-remove"]}
+              onClick={onRemoveHandler}
+            ></button>
+          </div>
+          <span className={classes["list-title"]}>{props.date}</span>
+          <span className={classes["list-content"]}>{props.item}</span>
+          <span className={classes["container-todo-button"]}>
+            {props.income}
+          </span>
+          <span className={classes["container-todo-button"]}>
+            {props.expend}
+          </span>
+          {totalSales ? <span>{totalSales}</span> : <></>}
+          <span>{props.memo}</span>
+        </li>
+      )}
     </>
   );
 };
