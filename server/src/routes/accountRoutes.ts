9ebc11eca1ab7c0721c6d6a1dc217a850650d9f0
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-04-12 21:35:50 +0900

    Account 서버 api 경로 추가

---
+++ server/src/routes/accountRoutes.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import controller from "../controllers/accountController.js";
+
+const router = express.Router();
+
+router.get("/", controller.getAccount);
+router.post("/create", controller.createAccount);
+router.get("/:id", controller.getAccountById);
+router.delete("/remove/:id", controller.deleteAccount);
+router.patch("/update/:id", controller.updateAccount);
+
+export default router;
