5448addab91262cb035f67e300e4cef73e30fa7e
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-04-14 07:36:06 +0900

    페이지네이션 버튼 컴포넌트 추가

---
+++ client/src/components/Button/PaginationButton.tsx
@@ -0,0 +1,25 @@
+import React, { Children } from "react";
+import classes from "./PaginationButton.module.css";
+
+interface Props {
+  className: string;
+  onClick: () => {};
+  disabled: boolean;
+  children: string;
+}
+
+const PaginationButton = (props: Props) => {
+  return (
+    <>
+      <button
+        className={classes[props.className]}
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >
+        {props.children}
+      </button>
+    </>
+  );
+};
+
+export default PaginationButton;
