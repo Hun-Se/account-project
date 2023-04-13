f0f88e165b42387004613417ea1e1aaa107d050e
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-04-14 07:40:47 +0900

    api 경로 상수화 파일 추가

--- client/src/constant/api_path_constant.ts
+++ client/src/constant/api_path_constant.ts
@@ -1,11 +1,11 @@
 const API_PATH = {
   LOGIN: "/api/users/login",
-  SIGNUP: "api/users/create",
-  TODOS: "/todos",
-  TODO_BY_ID: (id: string) => `/todos/${id}`,
-  CREATE_TODO: "/todos",
-  UPDATE_TODO: (id: string) => `/todos/${id}`,
-  DELETE_TODO: (id: string) => `/todos/${id}`,
+  SIGNUP: "/api/users/create",
+  ACCOUNT: "/api/accounts/",
+  ACCOUNT_BY_ID: (id: string) => `/api/accounts/${id}`,
+  CREATE_ACCOUNT: "/api/accounts/create",
+  UPDATE_ACCOUNT: (id: string) => `/api/accounts/update/${id}`,
+  DELETE_ACCOUNT: (id: string) => `/api/accounts/remove/${id}`,
 };
 
 export default API_PATH;
