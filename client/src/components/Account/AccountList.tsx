6fd22a11c81579f7d1424a07a3c1021be52aa828
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-04-24 12:04:29 +0900

    월별 label-selectbox option 수정

--- client/src/components/Account/AccountList.tsx
+++ client/src/components/Account/AccountList.tsx
@@ -73,18 +73,18 @@ const AccountList = () => {
               value={month}
               onChange={({ target: { value } }) => setMonth(Number(value))}
             >
-              <option value=""></option>
+              <option value="0">전체</option>
               <option value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
-              <option value="4">5</option>
-              <option value="4">6</option>
-              <option value="4">7</option>
-              <option value="4">8</option>
-              <option value="4">9</option>
-              <option value="4">10</option>
-              <option value="4">11</option>
-              <option value="4">12</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="11">11</option>
+              <option value="12">12</option>
             </select>
           </label>
           <ul className={classes["container-title"]}>
