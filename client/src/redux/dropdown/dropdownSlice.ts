25ea04e88a8dde75630e4eb815c4d6f0142ee679
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-04-24 11:41:56 +0900

    payload로 받은 데이터를 정렬방식에 따라 정렬하는 sorData 액션 추가

--- client/src/redux/dropdown/dropdownSlice.ts
+++ client/src/redux/dropdown/dropdownSlice.ts
@@ -1,12 +1,26 @@
+import { AccountType } from "./../../types/account";
 import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 
 export interface dropdownState {
-  isshown: boolean;
+  isShown: boolean;
+  isSortMeans: string;
+  isSortedAccount: [AccountType];
 }
 
 const initialState: dropdownState = {
-  isshown: false,
+  isShown: false,
+  isSortMeans: "",
+  isSortedAccount: [
+    {
+      _id: "",
+      date: "",
+      item: "",
+      income: "",
+      expend: "",
+      memo: "",
+    },
+  ],
 };
 
 export const dropdownSlice = createSlice({
@@ -14,13 +28,61 @@ export const dropdownSlice = createSlice({
   initialState,
   reducers: {
     showlist: (state) => {
-      state.isshown = !state.isshown;
+      state.isShown = !state.isShown;
+    },
+    sortData: (state, action) => {
+      state.isSortMeans = action.payload.item;
+      state.isSortedAccount = action.payload.account;
+      switch (action.payload.item) {
+        case "등록순":
+          state.isSortedAccount = action.payload.account.slice(0).reverse();
+          return;
+        case "최신순":
+          state.isSortedAccount = action.payload.account
+            .slice(0)
+            .sort(function (a: AccountType, b: AccountType) {
+              return new Date(b.date).getTime() - new Date(a.date).getTime();
+            });
+          return;
+        case "오래된순":
+          state.isSortedAccount = action.payload.account
+            .slice(0)
+            .sort(function (a: AccountType, b: AccountType) {
+              return new Date(a.date).getTime() - new Date(b.date).getTime();
+            });
+          return;
+        case "총합높은순":
+          state.isSortedAccount = action.payload.account
+            .slice(0)
+            .sort(function (a: AccountType, b: AccountType) {
+              return (
+                Number(b.income) -
+                Number(b.expend) -
+                (Number(a.income) - Number(a.expend))
+              );
+            });
+          return;
+        case "총합낮은순":
+          state.isSortedAccount = action.payload.account
+            .slice(0)
+            .sort(function (a: AccountType, b: AccountType) {
+              return (
+                Number(a.income) -
+                Number(a.expend) -
+                (Number(b.income) - Number(b.expend))
+              );
+            });
+          return;
+        default:
+          state.isSortedAccount = action.payload.account.slice(0).reverse();
+          return;
+      }
     },
   },
 });
 
-export const { showlist } = dropdownSlice.actions;
+export const { showlist, sortData } = dropdownSlice.actions;
 
-export const selectDropDownShown = (state: RootState) => state.dropdown.isshown;
+export const selectDropDown = (state: RootState) => state.dropdown;
 
 export default dropdownSlice.reducer;
