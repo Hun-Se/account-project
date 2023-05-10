372488695dc96a76674c2dd841015950f9c9f6f6
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-05-10 21:54:12 +0900

    메뉴 이동 버튼리스트들을 묶는 nav태그를 추가

--- client/src/components/Layout/Aside/Aside.tsx
+++ client/src/components/Layout/Aside/Aside.tsx
@@ -10,22 +10,29 @@ const Aside = () => {
     <>
       <aside className={classes["aside-container"]}>
         <div className={classes.logo}></div>
-        <ul>
-          <li
-            className={classes["aside-list"]}
-            onClick={() => navigate(ROUTES.DASHBOARD)}
-          >
-            대시보드
-          </li>
-          <li
-            className={classes["aside-list"]}
-            onClick={() => navigate(ROUTES.ACCOUNT)}
-          >
-            가계부 입력
-          </li>
-          <li className={classes["aside-list"]}>캘린더</li>
-          <li className={classes["aside-list"]}>세금계산봇</li>
-        </ul>
+        <nav>
+          <ul>
+            <li
+              className={classes["aside-list"]}
+              onClick={() => navigate(ROUTES.DASHBOARD)}
+            >
+              대시보드
+            </li>
+            <li
+              className={classes["aside-list"]}
+              onClick={() => navigate(ROUTES.ACCOUNT)}
+            >
+              가계부 입력
+            </li>
+            <li
+              className={classes["aside-list"]}
+              onClick={() => navigate(ROUTES.CALENDAR)}
+            >
+              캘린더
+            </li>
+            <li className={classes["aside-list"]}>세금계산봇</li>
+          </ul>
+        </nav>
       </aside>
     </>
   );
