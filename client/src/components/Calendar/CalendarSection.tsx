1ca7e398db2187124e92cbd2041308093c45b91d
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2023-05-16 23:53:09 +0900

    캘린더 main section UI 추가

--- client/src/components/Calendar/CalendarSection.tsx
+++ client/src/components/Calendar/CalendarSection.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import ControlDate from "./ControlDate";
+import DateBox from "./DateBox";
+import type { Holiday } from "../../types/holiday";
 import classes from "./CalendarSection.module.css";
 
 const CalendarSection = () => {
+  const [nowDate, setNowDate] = useState<Date>(new Date());
+  const [clickedDate, setClickedDate] = useState<Date>();
+  const [holiday, setholiday] = useState<Holiday[]>([]);
+
   return (
     <>
-      <h1 className={classes["body-title"]}>Calendar</h1>
+      <h1 className={classes["main-title"]}>Calendar</h1>
+      <div className={classes["container-section"]}>
+        <ControlDate nowDate={nowDate} setNowDate={setNowDate} />
+        <DateBox
+          nowDate={nowDate}
+          setNowDate={setNowDate}
+          clickedDate={clickedDate}
+          setClickedDate={setClickedDate}
+          holiday={holiday}
+        />
+      </div>
     </>
   );
 };
